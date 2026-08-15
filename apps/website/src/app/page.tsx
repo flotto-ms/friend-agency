763f43f56843b6dbb8bbdde0ad2b6e86f8c962db
Ady88 <mail@adrianeaton.co.uk>
2026-08-15 20:23:28 +0100

    root redirect

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,20 +1,5 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { UserSearch } from "@/components/UserSearch";
+import { redirect } from "next/navigation";
 
-export default function Account() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-[1200px] flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black">
-        <Card className="w-[350px] mx-auto">
-          <CardHeader>
-            <CardTitle>Sign In</CardTitle>
-            <CardDescription>Please sign in to view contractor details.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <UserSearch />
-          </CardContent>
-        </Card>
-      </main>
-    </div>
-  );
+export default function RootPage() {
+  redirect("/about");
 }
