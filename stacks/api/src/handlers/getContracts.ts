a68775fe02dac8631ccf57460eb7ac6c50616032
Ady88 <mail@adrianeaton.co.uk>
2026-08-17 08:37:51 +0100

    restore user active contracts

--- stacks/api/src/handlers/getContracts.ts
+++ stacks/api/src/handlers/getContracts.ts
@@ -1,25 +1,32 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import ContractTable from "../utils/ContractTable";
 import { ContractTableItem } from "@flotto/types";
+import ResponseUtils from "../utils/ResponseUtils";
+
+const activeContractMapping = (r: ContractTableItem[]) =>
+  r.map((c) => {
+    const { key, rateId, endedAt, ...rest } = c;
+    return { id: key, ...rest };
+  });
+
+const contractHistoryMapping = (r: ContractTableItem[]) =>
+  r.map((c) => {
+    const { key, userId, rateId, ...rest } = c;
+    return { ...rest };
+  });
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   const id = event.pathParameters?.id;
   let output: Partial<ContractTableItem>[] = [];
-
-  if (id) {
-    output = await ContractTable.getContractHistory(id).then((r) =>
-      r.map((c) => {
-        const { key, userId, rateId, ...rest } = c;
-        return { ...rest };
-      }),
-    );
+  if (id && event.path.includes("/users/")) {
+    if (!Number.isFinite(id)) {
+      return ResponseUtils.notFound("Unknown User");
+    }
+    output = await ContractTable.getActiveUserContracts(parseInt(id)).then(activeContractMapping);
+  } else if (id) {
+    output = await ContractTable.getContractHistory(id).then(contractHistoryMapping);
   } else {
-    output = await ContractTable.getActiveContracts().then((r) =>
-      r.map((c) => {
-        const { key, rateId, endedAt, ...rest } = c;
-        return { id: key, ...rest };
-      }),
-    );
+    output = await ContractTable.getActiveContracts().then(activeContractMapping);
   }
 
   return {
